ec460805d23d1e9ac47b54b22f66ba5aeb85be0e
Njeririziki <rizikinjeri@gmail.com>
2020-01-21 20:49:00 +0300

    Initial commit

---
+++ src/Pages/LogIn.js
@@ -0,0 +1,95 @@
+import React from 'react'
+import Container from '@material-ui/core/Container';
+import Avatar from '@material-ui/core/Avatar';
+import Button  from '@material-ui/core/Button';
+import Textfield from '@material-ui/core/TextField'
+import Typography from '@material-ui/core/Typography';
+import * as Icon from 'react-feather';
+import { makeStyles } from '@material-ui/core';
+
+const useStyle= makeStyles(theme=>({
+ body:{
+    backgroundColor:'#f2f2f2'  
+ },
+
+root:{
+    marginTop: theme.spacing(8),
+    display:'flex',
+    justifyContent:'center',
+    flexDirection:'column'
+},
+container:{
+    backgroundColor:'#f1f1f1'
+},
+avatar:{
+    alignSelf:'center',
+   width: theme.spacing(7),
+   height: theme.spacing(7),
+   backgroundColor:'#000000'
+},
+other:{
+marginTop:theme.spacing(3)
+},
+submit:{
+marginTop:theme.spacing(5),
+alignSelf:'center',
+backgroundColor:'#000000',
+
+}
+}))
+ 
+
+
+const LogIn = () => {
+    const classes =useStyle()
+    return (
+       <> 
+            <Container  maxWidth='xs'
+            className={classes.container}
+            >
+                <div className={classes.root}>
+               
+                <Avatar
+                className={classes.avatar}>
+                <Icon. LogIn/>
+                </Avatar>
+                
+                <form
+                 className={classes.other}>
+                    <Textfield
+                    variant='outlined'
+                    id='email'
+                    placeholder='Email'
+                    fullWidth
+                    className={classes.other}
+                    />
+                    <Textfield
+                    variant='outlined'
+                    id='password'
+                    placeholder='Password'
+                    fullWidth
+                    className={classes.other}
+                    />
+                    <Button
+                     className={classes.submit}
+                    variant='contained'
+                    style ={{fontFamily:'Titillium Web',
+                    color:'#ffffff',
+                     fontSize:20}}
+                    fullWidth
+                    >
+                        Log in 
+                    </Button>
+                </form>
+                <Typography
+                 className={classes.other}
+                >
+                    Create account
+                </Typography>
+                </div>
+            </Container>
+      </> 
+      );
+}
+ 
+export default LogIn;
